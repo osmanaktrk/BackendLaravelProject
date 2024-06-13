589da8daa6d288fe0e3da26fd81801ef29e8acc1
Osman <osmanaktrk@gmail.com>
2024-06-13 10:45:34 +0200

    factory fixed

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -19,11 +19,25 @@ class NewsFactory extends Factory
      */
     public function definition(): array
     {
+        $covers = [
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/fa03/live/feee5060-28d9-11ef-8c04-d9cb3ea38c14.png.webp',
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/76a9/live/916ee850-27ea-11ef-a13a-0b8c563da930.jpg.webp',
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/ea0b/live/0422b480-293a-11ef-98de-d7e44bcb3f0f.jpg.webp',
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/4554/live/12bc4980-291b-11ef-a08a-0dd98a792d5e.jpg.webp',
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/a389/live/19dee020-291a-11ef-8725-878da4c62a60.png.webp',
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/ff01/live/747e5370-2916-11ef-ae2c-87a203e6db8a.png.webp',
+            'https://ichef.bbci.co.uk/news/1536/cpsprodpb/6780/live/abdea470-271b-11ef-b2a2-8dd2c3838cf1.jpg.webp'
+            
+        ];
+        
+
+
         return [
             'user_id' => User::factory(),
             'category_id'=> Category::factory(),
             'title' => fake()->text(200),
-            'cover' =>  'img/news/demo.webp',
+            'cover' =>  fake()->randomElement($covers),
+            // 'cover' =>  'img/news/demo.webp',
             'content' => fake()->text(1500),
             
         ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
 
+        $avatars = [
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=3149&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1546961329-78bef0414d7c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        ];
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
@@ -32,7 +42,8 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
             'usertype' => fake()->randomElement(['user', 'admin', 'writer']),
             'is_banned' => fake()->boolean,
-            'avatar' => 'img/avatars/default.svg',
+            'avatar' => fake()->randomElement($avatars),
+            // 'avatar' => 'img/avatars/default.svg',
             
 
         ];
